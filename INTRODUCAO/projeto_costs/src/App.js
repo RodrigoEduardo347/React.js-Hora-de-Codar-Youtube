efb12ae7744a473b931dc2e05564db2914376e2e
unknown <rodrigo@opytsuporte.com.br>
2022-10-28 11:43:51 -0300

    NavBar e Footer estruturados para a versão web

--- INTRODUCAO/projeto_costs/src/App.js
+++ INTRODUCAO/projeto_costs/src/App.js
@@ -1,6 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layouts/Container'
+import NavBar from './components/layouts/NavBar'
+import Footer from './components/layouts/Footer'
+import Projects from './components/pages/Projects'
+
 function App() {
   return (
-    <h1>teste</h1>
+    <Router>
+      <NavBar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
